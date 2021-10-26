ba1f9063afbe2462248e4bf2c0eaa194eeda1f8f
giorgirogava <giorgirogava01ai@gmail.com>
2021-10-26 17:34:24 +0400

    init: start midterm-task

---
+++ Concentration/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by IMAC on 24.10.21.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}

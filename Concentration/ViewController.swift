b21c801c9229660df8bde04f75222079cb7ad6de
giorgirogava <giorgirogava01ai@gmail.com>
2021-10-27 17:35:55 +0400

    - fix: change ViewController class

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -69,18 +69,19 @@ class ViewController: UIViewController {
     }
     
     private var emojiChoices = [ "🪐", "🌍", "🛰", "🛸", "🖥", "🔭"]
+    //    private var emojiChoices = [ "🪐🌍🛰🛸🖥🔭"]
     
-    private var emoji = [Int:String]()
+    private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
         
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+        if emoji[card] == nil, emojiChoices.count > 0 {
             
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
             
         }
         
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
         
     }
     
